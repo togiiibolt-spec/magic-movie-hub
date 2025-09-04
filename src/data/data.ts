b9e026463793f57896218043e4e2f6fc0c36a30b
togiiibolt-spec <togiii.bolt@gmail.com>
2025-09-05 00:07:40 +0500

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -148,7 +148,7 @@ export const series: Series[] = [
         duration: '30m',
         episodeNumber: 1,
         seasonNumber: 1,
-        videoUrl: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+        videoUrl: 'https://play.zephyrflick.top/video/ec4de54f2f3afa14175e5eabfc16ce1f'
       }
     ],
     type: 'series'
